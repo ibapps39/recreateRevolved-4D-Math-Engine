2541ddd937d251a32693b231c00335d76723d792
Ian Brown <ibapps39@gmail.com>
2021-06-12 12:01:20 -0600

    Initial Commit

---
+++ recreateRevolved 4D Math Engine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  recreateRevolved 4D Math Engine
+//
+//  Created by Ian Brown on 6/12/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
